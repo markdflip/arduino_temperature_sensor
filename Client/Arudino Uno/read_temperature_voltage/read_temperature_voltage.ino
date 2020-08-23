402dcd471a1754535b9a682e139b0753cff009bc
markdflip <whitesoxfanatic@gmail.com>
2020-08-23 17:51:12 -0400

    Using millisecond loop to avoid using delay function.

--- Client/Arudino Uno/read_temperature_voltage/read_temperature_voltage.ino
+++ Client/Arudino Uno/read_temperature_voltage/read_temperature_voltage.ino
@@ -1,18 +1,23 @@
-
 #include <SoftwareSerial.h>
 
-SoftwareSerial fromESP(7,8); //Rx, Tx
+SoftwareSerial toESP(7, 8); //Rx, Tx
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis;
+long interval = 15000; //Length of time to delay in ms
 
 void setup()
 {
-  fromESP.begin(9600);
+  toESP.begin(9600);
 }
 
 void loop()
 {
-  fromESP.print(analogRead(2));
-  fromESP.print('\n');
-
-  delay(2000);
-
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval)
+  {
+    toESP.print(analogRead(2));
+    toESP.print('\n');
+    previousMillis = currentMillis;
+  }
 }
