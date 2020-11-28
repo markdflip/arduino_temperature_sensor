a799796513ee0ab8ffd1eb8eb327dfe04ba02263
mark.j.difilippo <mark.j.difilippo@accenture.com>
2020-11-28 17:50:40 -0500

    Updating comment coverage + code to enable debug.

--- Client/Arudino Uno/read_temperature_voltage/read_temperature_voltage.ino
+++ Client/Arudino Uno/read_temperature_voltage/read_temperature_voltage.ino
@@ -1,23 +1,34 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial toESP(7, 8); //Rx, Tx
+SoftwareSerial toESP(7, 8); //Rx, Tx pins over software serial (not TX and RX physical pins).
 
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
-long interval = 15000; //Length of time to delay in ms
+long interval = 600000; //Length of time to delay in ms. Set to 10 minutes.
 
 void setup()
 {
+  //Serial.begin(19200); //Enable this line to enable debug in serial monitor.
   toESP.begin(9600);
 }
 
 void loop()
 {
+
+/*
+  To avoid using delay(), the time is compared to see if the 
+  specified interval has been passed. Once the time passes,
+  the Arduino sends the ADC from the temperature sensor
+  to the ESP over the soft serial using baud rate initialized
+  in setup().
+*/
+  
   currentMillis = millis();
   if (currentMillis - previousMillis > interval)
   {
     toESP.print(analogRead(2));
     toESP.print('\n');
+    //Serial.println(analogRead(2)); //Enable to debug in serial monitor.
     previousMillis = currentMillis;
   }
 }
